9e5f5955222dac13c169ce9935f327942c12331b
Bharat <hibharatj@gmail.com>
2025-06-06 16:50:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function shortcut(s1, s2) {
-  // your code here
+    let ans = '';
+    
+    if(s1.length > 0) {
+        ans +=s1[0];
+    }
+    
+    if(s2.length > 0) {
+        ans +=s2[0];
+    }
+    
+    return ans;
+    
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
